379b8b1fe0c8ec158a095e73b8ead6c19dc4191b
Brandon <brandon_scott585@yahoo.com>
2022-08-29 14:17:02 -0700

    adding

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-        continue
-    print("{:c}".format(alpha_letters), end="")
+for ch in range(97, 123):
+    if (ch != 101 and ch != 113):
+        print('{:c}'.format(ch), end="")
