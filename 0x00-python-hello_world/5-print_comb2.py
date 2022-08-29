4025ba754659e4421db3458da6fdc7425f52b27f
Brandon <brandon_scott585@yahoo.com>
2022-08-29 15:22:22 -0700

    hexadicimals??

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for j in range(100):
+for j in range(0, 100):
     if j < 99:
-        print("{:02d},".format(j), end=' ')
-    elif 1 == 99:
-        print("{:d}".format(j))
+        print("{:02d},".format(j), end="")
+    else:
+        print("{:02d}\n".format(j), end="")
