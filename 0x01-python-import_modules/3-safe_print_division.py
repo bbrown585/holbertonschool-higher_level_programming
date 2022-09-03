246ad6505c734c5490786b5198188df85af646e2
Brandon <brandon_scott585@yahoo.com>
2022-09-02 20:16:43 -0700

    retry task 3

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    a = 5
-    b = 0
-    print("{} + {} = {}".format(a, b, (add(a, b))))
+    try:
+        c = a / b
+    except ZeroDivisionError:
+        c = None
+    finally:
+        print("Inside result: {}".format(c))
+        return c
