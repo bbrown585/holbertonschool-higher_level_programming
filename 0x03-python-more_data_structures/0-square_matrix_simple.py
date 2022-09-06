9473b372ab223880edd7d09e17670bc018dc35d3
Brandon <brandon_scott585@yahoo.com>
2022-09-06 11:28:16 -0700

    task 0

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
+    sqmatrix = []
+    for i in range(len(sqmatrix)):
+        sqmatrix[i] = list(map(lambda a: a ** 2, sqmatrix[i]))
+        return (sqmatrix)
