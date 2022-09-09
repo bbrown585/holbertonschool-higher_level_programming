fd112d2cf856f17b9b4805ea25a02b625b2330ab
Brandon <brandon_scott585@yahoo.com>
2022-09-08 19:26:47 -0700

    roman num

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    
+    if type(roman_string) != str or not roman_string:
+        return(0)
+    values = {'I':1, 'V':5, 'X':10, 'L':50, 'L':100, 'D':500, 'M':1000}
+    sum = 0
+    last = 0
+
+    for i in range(len(roman_string)):
+        if last != 0 and last < values [roman_string[i]]:
+            sum -= last
+        else:
+            sum += last
+            last = values [roman_string[i]]
+            sum += last
+            return (sum)
+
