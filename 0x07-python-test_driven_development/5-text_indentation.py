9887304ed55ffb52482c4fd80032b8e0458217ae
Brandon <brandon_scott585@yahoo.com>
2022-09-12 18:54:39 -0700

    trying task 3 again

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""2 Lines Module"""
+
+
+def text_indentation(text):
+    """Text indentation"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
