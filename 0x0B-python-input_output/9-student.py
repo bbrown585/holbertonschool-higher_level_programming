016c9317b409555b2a1e540cf13411f263eb3c44
Brandon <brandon_scott585@yahoo.com>
2022-09-19 19:01:12 -0700

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Student class module"""
+"""JSON student module"""
 
 
-class Student():
-    """defines a Student class"""
+class Student:
+    """ Student Class """
     def __init__(self, first_name, last_name, age):
-         """init method"""
+        """ Init method """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """return Student"""
-        return self.__dict__.copy()
+        """ return Student"""
+        return self.__dict__
