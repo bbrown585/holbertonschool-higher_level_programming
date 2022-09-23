616ba5366a0f1f76461333d5c9feac0aebde58cb
Brandon <brandon_scott585@yahoo.com>
2022-09-23 16:59:01 -0700

    task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError("width must be > 2")
         self.__width = value
 
     @property
