0b12628d8cff3ca5eadaff827e1b81b1d863c5e6
Brandon <brandon_scott585@yahoo.com>
2022-09-23 19:30:37 -0700

    task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,3 +15,85 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+        
+        @property
+    def width(self):
+        '''get width of a rectangle'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''set width of a rectangle'''
+        self.validate_integer("width", value, False)
+        self.__width = value
+
+    @property
+    def height(self):
+        '''get height of a rectangle'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''set height of a rectangle'''
+        self.validate_integer("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''get x of a rectangle'''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''set x of a rectangle'''
+        self.validate_integer("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''get y of a rectangle'''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''set y of a rectangle'''
+        self.validate_integer("y", value)
+        self.__y = value
+
+    def validate_integer(self, name, value, eq=True):
+        '''check the value as int and >= 0'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    def area(self):
+        '''find area of a rectangle'''
+        return self.width * self.height
+
+    def display(self):
+        '''to print string rep of a rectangle'''
+        rep = '\n' * self.y + \
+            (' ' * self.x + '#' * self.width + '\n') * self.height
+        print(rep, end='')
+
+    def __str__(self):
+        '''str info about a rectangle'''
+        return '[{}] ({}) {}/{} - {}/{}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width,
+                   self.height)
+
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''update instance attributes via */**args'''
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
