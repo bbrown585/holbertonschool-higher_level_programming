86360b9d4718d514c213d799a309471041742d64
Brandon <brandon_scott585@yahoo.com>
2022-09-23 21:31:37 -0700

    task 10

---
+++ 0x0C-python-almost_a_circle/tests/test_modules/test_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest base.py"""
+
+import unittest
+from models.base import Base
+from models.square import Rectangle
+
+
+class TestSquare(unittest.TestCase):
+    """unittest"""
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
