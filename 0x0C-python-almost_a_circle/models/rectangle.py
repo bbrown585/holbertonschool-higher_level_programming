da8a9d195ba160ede1e700bda5c4e6e98fcf4859
Brandon <brandon_scott585@yahoo.com>
2022-09-23 19:05:29 -0700

    task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module Rectangle
+    creates a Rectangle that inherits from Base """
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ describes a Rectangle """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ initializes a Rectangle instance """
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
