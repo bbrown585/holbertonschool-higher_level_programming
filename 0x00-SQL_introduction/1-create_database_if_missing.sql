1504efada4af798971d71b2567041343e79cdec4
bbrown585 <brandon_scott585@yahoo.com>
2022-10-05 13:07:54 -0700

    MySQL database

---
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- write a script that createts the databse
+-- hbtcn_0c_0 in your my MySQL server
+DROP DATABASE IF EXISTS hbtcn_0c_0;
+CREATE DATATBASE hbtcn_0c_0;
---
+++ 0x00-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Write a script that deletes the database
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+DROP DATABSE IF EXISTS hbtn_0c_0;
