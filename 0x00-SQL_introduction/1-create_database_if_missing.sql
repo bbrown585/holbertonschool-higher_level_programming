4b434fcee1d549a1ad374882db1b503d68d1ef35
bbrown585 <brandon_scott585@yahoo.com>
2022-10-05 13:11:02 -0700

    MySQL database

--- 0x00-SQL_introduction/1-create_database_if_missing.sql
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- write a script that createts the databse
 -- hbtcn_0c_0 in your my MySQL server
-DROP DATABASE IF EXISTS hbtcn_0c_0;
-CREATE DATATBASE hbtcn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATATBASE hbtn_0c_0;
