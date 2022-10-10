04113ad588aea3fd39072661fec2663dee3d4453
Brandon <brandon_scott585@yahoo.com>
2022-10-10 10:45:02 -0500

    my sql

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@
 SELECT tv_shows.title,tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY 1,2;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
+
