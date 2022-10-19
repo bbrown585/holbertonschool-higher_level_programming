fb49c7405ec6888152e0078ec679a124c0383952
bbrown585 <brandon_scott585@yahoo.com>
2022-10-19 09:18:20 -0700

    task 0

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bim/python3
+""" a script that lists all states from the database hbtn_0e_0_usa"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    kerr = conn.cursor()
+    kerr.execute("SELECT * FROM states ORDER BY id ASC")
+    query_table = kerr.fetchall()
+    for row in query_table:
+        print(row)
+    kerr.close()
+    conn.close()
