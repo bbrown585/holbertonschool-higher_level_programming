4fc76d171c555977877d7e123bee5b9d726b7ea1
bbrown585 <brandon_scott585@yahoo.com>
2022-10-19 17:08:53 -0700

    task 3

---
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a script that lists all states with a name starting \
+    with N (upper N) from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """Document for"""
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    kerr = conn.cursor()
+    kerr.execute("SELECT * FROM states WHERE Name =  %s ORDER BY id ASC",
+                 [sys.argv[4]])
+    query_table = kerr.fetchall()
+    for row in query_table:
+        print(row)
+    kerr.close()
+    conn.close()
