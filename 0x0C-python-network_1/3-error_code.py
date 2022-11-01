df0d34bf43e25b521186ebacf4c66ed5a1ce3c59
bbrown585 <brandon_scott585@yahoo.com>
2022-11-01 16:16:13 -0700

    task 3 attempt

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""request to the URL and displays the body of the response (decoded in utf-8)."""
+import sys
+from urllib import request, error
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as f:
+            print(f.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
