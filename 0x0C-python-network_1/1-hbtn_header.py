6c6198217ce0dfc57c20e85131a5c901ddb14586
bbrown585 <brandon_scott585@yahoo.com>
2022-11-01 14:09:04 -0700

    task 1

---
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a doc for sends a request to the URL and displays the value of the X-Request-ID"""
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
