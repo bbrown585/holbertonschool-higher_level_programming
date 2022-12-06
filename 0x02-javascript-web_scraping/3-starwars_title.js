77c5e66a5734b0e9f34aabb3f26476bda2fc7ae3
bbrown585 <brandon_scott585@yahoo.com>
2022-12-06 14:12:42 -0800

    retry task 3

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-let resultDict = {};
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    resultDict = JSON.parse(body);
-    console.log(resultDict.title);
-  }
+const [id] = process.argv.slice(2);
+request(`https://swapi-api.hbtn.io/api/films/${id}`, (err, res, body) => {
+  if (err) console.error(err);
+  console.log(JSON.parse(body).title);
 });
