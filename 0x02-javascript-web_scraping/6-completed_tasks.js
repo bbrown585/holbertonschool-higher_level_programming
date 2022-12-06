b1eaaeb7e60ce30c1c27d055974947995b14dadf
bbrown585 <brandon_scott585@yahoo.com>
2022-12-06 15:32:46 -0800

    task 6 semi

---
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function callback (error, response, body) {
+  if (error) throw error;
+  const users = {};
+  for (const user of JSON.parse(body)) {
+    if (user.completed) {
+      if (users[user.userId]) {
+        users[user.userId]++;
+      } else {
+        users[user.userId] = 1;
+      }
+    }
+  }
+  console.log(users);
+}
+
+request(process.argv[2], callback);
