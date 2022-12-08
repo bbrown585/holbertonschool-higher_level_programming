5d703075f85a70acda8616c3be331288ee02dacf
bbrown585 <brandon_scott585@yahoo.com>
2022-12-07 17:05:22 -0800

    task 7-9

---
+++ 0x03-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+const $ = window.$;
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+$.getJSON(url, function (response) {
+  $.each(response.results, function (i, item) {
+    $('UL#list_movies').append('<li>' + item.title + '</li>');
+  });
+});
